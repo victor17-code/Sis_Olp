0e8863933f1c3209ed46943b64b39ae1939f10c6
victor17-code <2016110312@udh.edu.pe>
2023-01-25 15:32:33 -0500

    instalando mis panel de administracion 2

---
+++ vistas/modulos/menu.php
@@ -0,0 +1,38 @@
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="vistas/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+            </div>
+            <div class="pull-left info">
+                <p>Victor Eusebio Soto</p>
+                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            </div>
+        </div>
+
+        <ul class="sidebar-menu" data-widget="tree">
+            <li class="header">MENU DE NAVEGACION</li>
+
+            <!-- ===============MENU INICIO=================== -->
+            <li><a href="#"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
+
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-edit"></i> <span>Mantenimiento</span>
+                    <span class="pull-right-container">
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="#"><i class="fa fa-circle-o"></i>Usuarios</li>                  
+                </ul>
+            </li>
+
+            <li class="header">LABELS</li>
+            <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
+        </ul>
+    </section>
+    <!-- /.sidebar -->
+</aside>
